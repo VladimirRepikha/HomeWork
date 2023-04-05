0b1d2e62ddb81e017c7de9c7dc770589da758c4e
VladimirRepikha <106553330+VladimirRepikha@users.noreply.github.com>
2023-04-05 14:24:18 +0600

    Удаление лишних строк.

--- C#/less3/task19/Program.cs
+++ C#/less3/task19/Program.cs
@@ -5,26 +5,25 @@
 // 23432 -> да
 
  Console.Write("Введите число: ");
-        int number = Convert.ToInt32(Console.ReadLine());
+ string str = Console.ReadLine();
 
-        string str = number.ToString();
-        int length = str.Length;
-        bool isPalindrome = true;
+ int length = str.Length;
+ bool isPalindrome = true;
 
-        for (int i = 0; i < length / 2; i++)
-        {
-            if (str[i] != str[length - i - 1])
-            {
-                isPalindrome = false;
-                break;
-            }
-        }
+ for (int i = 0; i < length / 2; i++)
+ {
+     if (str[i] != str[length - i - 1])
+     {
+         isPalindrome = false;
+         break;
+     }
+}
 
-        if (isPalindrome)
-        {
-            Console.WriteLine("Число является палиндромом");
-        }
-        else
-        {
-            Console.WriteLine("Число не является палиндромом");
-        }
+if (isPalindrome)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
