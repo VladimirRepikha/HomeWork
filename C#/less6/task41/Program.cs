e33b7b48199cb3711b65ac2e86f47b0759793b13
Vladimir <vrepikha@inbox.ru>
2023-04-08 12:18:28 +0300

    Решение задачи 41

---
+++ C#/less6/task41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Сколько чисел вы хотите ввести? ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Количество чисел больше нуля: {count}");
