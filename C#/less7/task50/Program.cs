820ab3dd4ca008f58aead61881a8fdbe944d2e58
Vladimir <vrepikha@inbox.ru>
2023-04-11 17:02:50 +0300

    Решение задачи 50

---
+++ C#/less7/task50/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int[,] array = { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+Console.Write("В какой строке искать? ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("В каком столбце искать? ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m < 0 || m >= array.GetLength(0) || n < 0 || n >= array.GetLength(1))
+{
+    Console.WriteLine("В указаных координатах ничего не найдено.");
+}
+else
+{
+    int element = array[m, n];
+    Console.WriteLine($"Значение элемента [{m}, {n}]: {element}");
+}
