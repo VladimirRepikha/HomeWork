1bccdb4f59ed32b472390b7bea794f2dc03b22c5
Vladimir <vrepikha@inbox.ru>
2023-04-12 10:37:42 +0300

    Решение задачи 54

---
+++ C#/less8/task54/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] arr = new int[,] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        for (int k = j + 1; k < arr.GetLength(1); k++)
+        {
+            if (arr[i, k] > arr[i, j])
+            {
+                int temp = arr[i, j];
+                arr[i, j] = arr[i, k];
+                arr[i, k] = temp;
+            }
+        }
+    }
+}
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
